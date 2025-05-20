a66cec41d43ab5cd039387f06bfec53544c7e060
te220618 <tejc220618@gmail.com>
2025-05-20 10:37:05 +0900

    コミットメッセージ

---
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/DemoApplication.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp5.mybootapp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
@@ -0,0 +1,30 @@
+package jp.te4a.spring.boot.myapp5.mybootapp5;
+
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Hello world!
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg", "this is a setting message");
+        // return "Hello, Spring Boot!";
+        return "index";
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("text1") String text1) {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("msg", "you write '" + text1 + "'!!!");
+        return mv;
+    }
+}
