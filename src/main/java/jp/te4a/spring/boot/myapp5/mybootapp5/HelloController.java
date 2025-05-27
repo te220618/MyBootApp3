a8a980e624408ef320bb4cb26a80272123a7d389
te220618 <tejc220618@gmail.com>
2025-05-27 11:45:33 +0900

    コミットメッセージ

--- src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
@@ -13,18 +13,9 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class HelloController {
-
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("msg", "this is a setting message");
-        // return "Hello, Spring Boot!";
+        // return "this is Spring Boot sample.";
         return "index";
-    }
-
-    @RequestMapping(value = "/post", method = RequestMethod.POST)
-    public ModelAndView postForm(@RequestParam("text1") String text1) {
-        ModelAndView mv = new ModelAndView("index");
-        mv.addObject("msg", "you write '" + text1 + "'!!!");
-        return mv;
     }
 }
