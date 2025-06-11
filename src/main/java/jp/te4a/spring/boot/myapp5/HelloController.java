cf76a9f14dca371ece0563d6570f4a16cb181290
te220618 <tejc220618@gmail.com>
2025-06-11 12:55:13 +0900

    課題5コミット

--- src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp5.mybootapp5;
+package jp.te4a.spring.boot.myapp5;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
